43f7c159ec7ff321d30225be93b11940b96684aa
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2026-03-21 14:18:54 -0300

    feat: ajustes 210325 as 1415

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,3 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'sobre',
-    pathMatch: 'full',
-  },
-  {
-    path: 'sobre',
-    loadComponent: () => import('./pages/sobre/sobre').then((m) => m.Sobre),
-  },
-  {
-    path: 'stacks',
-    loadComponent: () => import('./pages/stacks/stacks').then((m) => m.Stacks),
-  },
-  {
-    path: 'projetos',
-    loadComponent: () => import('./pages/projetos/projetos').then((m) => m.Projetos),
-  },
-];
+export const routes: Routes = [];
--- src/app/components/header/header-logo/header-logo.ts
+++ src/app/components/header/header-logo/header-logo.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
 import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-header-logo',
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule],
   templateUrl: './header-logo.html',
   styleUrl: './header-logo.css',
 })
